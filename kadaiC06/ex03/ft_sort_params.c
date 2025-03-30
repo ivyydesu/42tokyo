a038d1119f057c47dc5b4aadc88bc67d8bb4a0d7
Aoki kotaro <aokikotaru@icloud.com>
2025-03-30 09:39:04 +0900

    dddd

---
+++ kadaiC06/ex03/ft_sort_params.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaoki <kaoki@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/28 16:27:35 by kaoki             #+#    #+#             */
+/*   Updated: 2025/03/28 17:11:44 by kaoki            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	strcmp_custom(char *str1, char *str2)
+{
+	int	i;
+
+	i = 0;
+	while (str1[i] != '\0' && str2[i] != '\0')
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (str1[i] - str2[i]);
+}
+
+void	bubble_sort(char *argv[], int argc)
+{
+	int		i;
+	char	*temp;
+	int		j;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (strcmp_custom(argv[i], argv[j]) > 0)
+			{
+				temp = argv[i];
+				argv[i] = argv[j];
+				argv[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+void	print_string(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	write(1, str, i);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	i = 1;
+	if (argc <= 1)
+		return (0);
+	bubble_sort(argv, argc);
+	while (i < argc)
+	{
+		print_string(argv[i]);
+		i++;
+	}
+	return (0);
+}
